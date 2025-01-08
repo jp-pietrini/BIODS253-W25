5394baca888032dca90e89ee73b300aa12a57bb1
Juan Pablo Pietrini <jpietrin@stanford.edu>
2025-01-08 13:03:06 -0800

    Update Assignement 1.py

---
+++ Assignement 1.py
@@ -0,0 +1,22 @@
+## Assignment 1:
+# The objective is to create a Pascal's trigle for the first 15 lines of the Pascal's Triangel
+
+# I will start with a loop
+
+def print_pascal_triangle(n):
+    triangle = [[1]]
+
+    for i in range(1,n):
+        row = [1]
+        for j in range (1,i):
+            row.append(triangle[i-1][j-1] +triangle[i-1][j])
+
+        row.append(1)
+        triangle.append(row)
+    
+    #Print the triangle
+    max_width = len(''.join(map(str,triangle[-1])))
+    for row in triangle:
+        print(''.join(map(str, row)).center(max_width))
+
+    print_pascal_triangle
